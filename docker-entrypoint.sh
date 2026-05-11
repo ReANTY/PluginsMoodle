40a7a60bfd3ee14be15c45389c1b36a3c8458e6b
ReANTY <reanta@example.com>
2026-05-12 01:53:02 +0700

    Fix moodledata permissions in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,5 +9,10 @@ rm -f /etc/apache2/mods-enabled/mpm_*.conf
 ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
 ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
 
+# Ensure moodledata directory exists and has correct permissions
+mkdir -p /app/moodledata
+chown -R www-data:www-data /app/moodledata
+chmod -R 0777 /app/moodledata
+
 # Start Apache
 exec apache2-foreground
